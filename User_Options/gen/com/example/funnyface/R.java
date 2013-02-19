80ce0efe4c60a1fcd2ccb2bc1a20051ca643d1c3
Victor <tieu.victor@gmail.com>
2013-02-19 18:23:23 -0500

    Added images for icons, home screen
    will be fixing some other screens for easier navigation

--- User_Options/gen/com/example/funnyface/R.java
+++ User_Options/gen/com/example/funnyface/R.java
@@ -11,8 +11,16 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int moustache=0x7f020001;
+        public static final int camera=0x7f020000;
+        public static final int clickededit=0x7f020001;
+        public static final int clickedgallery=0x7f020002;
+        public static final int clickedhome=0x7f020003;
+        public static final int edit=0x7f020004;
+        public static final int gallery=0x7f020005;
+        public static final int home=0x7f020006;
+        public static final int homescreen=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int moustache=0x7f020009;
     }
     public static final class id {
         public static final int button1=0x7f070000;
@@ -22,9 +30,12 @@ public final class R {
         public static final int button5=0x7f070007;
         public static final int button6=0x7f070008;
         public static final int button7=0x7f07000a;
+        public static final int gallery=0x7f07000d;
+        public static final int home=0x7f07000b;
         public static final int horizontalScrollView1=0x7f070002;
         public static final int imageView1=0x7f070009;
-        public static final int menu_settings=0x7f07000b;
+        public static final int menu_settings=0x7f07000e;
+        public static final int picture=0x7f07000c;
         public static final int textView1=0x7f070003;
         public static final int textView2=0x7f070004;
     }
