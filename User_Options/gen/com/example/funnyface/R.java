f6ea11153b1fe09960efc77392b538c5d8912391
Victor <tieu.victor@gmail.com>
2013-02-20 14:58:50 -0500

    added background to userphotoselection

--- User_Options/gen/com/example/funnyface/R.java
+++ User_Options/gen/com/example/funnyface/R.java
@@ -11,16 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int camera=0x7f020000;
-        public static final int clickededit=0x7f020001;
-        public static final int clickedgallery=0x7f020002;
-        public static final int clickedhome=0x7f020003;
-        public static final int edit=0x7f020004;
-        public static final int gallery=0x7f020005;
-        public static final int home=0x7f020006;
-        public static final int homescreen=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
-        public static final int moustache=0x7f020009;
+        public static final int background=0x7f020000;
+        public static final int camera=0x7f020001;
+        public static final int clickededit=0x7f020002;
+        public static final int clickedgallery=0x7f020003;
+        public static final int clickedhome=0x7f020004;
+        public static final int edit=0x7f020005;
+        public static final int gallery=0x7f020006;
+        public static final int home=0x7f020007;
+        public static final int homescreen=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int moustache=0x7f02000a;
     }
     public static final class id {
         public static final int button1=0x7f070000;
