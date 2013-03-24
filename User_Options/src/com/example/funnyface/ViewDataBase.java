c9427209deb1bc2397a70c4687b1ca1526786da4
Fadi Younis <fadi.younis65@gmail.com>
2013-03-24 03:04:32 -0400

    Web view set up to connect to Rails upon connection

--- User_Options/src/com/example/funnyface/ViewDataBase.java
+++ User_Options/src/com/example/funnyface/ViewDataBase.java
@@ -1,23 +1,56 @@
 package com.example.funnyface;
 
-import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
 import android.view.Menu;
+import android.webkit.WebView;
+import android.widget.Toast;
 
-public class ViewDataBase extends Activity {
-
+public class ViewDataBase extends Activity 
+{
+	 WebView cloudConnection;
+	@SuppressLint("SetJavaScriptEnabled")
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_data_base);
+		ConnectivityManager cloudconnection = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+		try
+		{
+			if(cloudconnection.getActiveNetworkInfo( ) != null && cloudconnection.getActiveNetworkInfo( ).isAvailable() && cloudconnection.getActiveNetworkInfo( ).isConnected( ))
+			{	
+				Toast.makeText(getApplicationContext(), "Connecting ...",Toast.LENGTH_LONG).show();
+				cloudConnection = (WebView) findViewById(R.id.webView1);
+				cloudConnection.getSettings().setJavaScriptEnabled(true);
+				cloudConnection.getSettings().setBuiltInZoomControls(true);
+				cloudConnection.loadUrl("http://editmenow.herokuapp.com/users");
+			}
+			else
+			{
+					Toast.makeText(getApplicationContext(), "No internet connection, try again.",Toast.LENGTH_LONG).show();
+			}
+		}
+		catch(Exception e)
+		{
+			
+		}
 	}
 
+
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		//getMenuInflater().inflate(R.menu.activity_view_data_base, menu);
 		return true;
 	}
+	
+	
 
 }
